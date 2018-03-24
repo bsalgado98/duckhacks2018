e50c44c3a45ff1483dcc6622f3da13dc76123144
HienBui711 <hbui@stevens.edu>
2018-03-24 19:57:48 -0400

    change opacity

--- js/homejavascript.js
+++ js/homejavascript.js
@@ -1,6 +1,6 @@
 function onInput1() {
-    let user_input = document.getElementById("problem").value; 
-    var question = document.getElementById("second_question")
-    question.style.opacity = "1";
-    question.style.transition = "opacity 0.5s";
+//    let user_input = document.getElementById("problem").value; 
+//    var question = document.getElementById("second_question")
+    document.getElementById("form2").style.opacity = "1";
+    document.getElementById("form2").style.transition = "opacity 0.5s";
 }
