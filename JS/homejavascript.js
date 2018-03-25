6eee6748bffa065b84588cdc9d763ec034901c54
HienBui711 <hbui@stevens.edu>
2018-03-25 01:13:18 -0400

    a

--- JS/homejavascript.js
+++ JS/homejavascript.js
@@ -3,20 +3,21 @@ function FadeIn() {
     document.getElementById("form1").style.transition = "opacity 1s";
 }
 
-function getInput() {
-    //let user_input = document.getElementById("problem").value; 
-    let user_input = "something user said"
-    alert(user_input);
-}
+//function getInput() {
+//    let user_input = document.getElementById("problem").value; 
+//    let user_input = "something user said"
+//    alert(user_input);
+//}
 
 
 function onInput1() {
-    let user_input = document.getElementById("problem").value; 
+    //let user_input = document.getElementById("problem").value; 
     document.getElementById("form2").style.opacity = "1";
     document.getElementById("form2").style.transition = "opacity 1s";
 }
 
 function FadeIn2() {
-    
+    document.getElementById("p1").style.opacity = "1";
+    document.getElementById("p1").style.transition = "opacity 1s";
 }
 
