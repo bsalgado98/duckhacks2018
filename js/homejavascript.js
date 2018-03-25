f2fe55902a228c88004889d31fde535232b1ceb4
HienBui711 <hbui@stevens.edu>
2018-03-24 20:54:02 -0400

    changed css of homepage

--- js/homejavascript.js
+++ js/homejavascript.js
@@ -1,6 +1,11 @@
-function onInput1() {
-//    let user_input = document.getElementById("problem").value; 
-//    var question = document.getElementById("second_question")
-    document.getElementById("form2").style.opacity = "1";
-    document.getElementById("form2").style.transition = "opacity 0.5s";
+function FadeIn() {
+    document.getElementById("form1").style.opacity = "1";
+    document.getElementById("form1").style.transition = "opacity 1s";
 }
+
+function onInput1() {
+    let user_input = document.getElementById("problem").value; 
+    document.getElementById("form2").style.opacity = "1";
+    document.getElementById("form2").style.transition = "opacity 1s";
+}
+
