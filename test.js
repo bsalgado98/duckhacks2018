9d895b8e7fbfa1cfbb112d198d68fedd3986ef92
Ookayleb <cchoy@stevens.edu>
2018-03-25 02:37:28 -0400

    cdn

---
+++ test.js
@@ -0,0 +1,87 @@
+// $(document).ready(function(){
+//     $("button").click(function(){
+//         $.post("demo_test_post.asp",
+//         {
+//           name: "Donald Duck",
+//           city: "Duckburg"
+//         },
+//         function(data,status){
+//             alert("Data: " + data + "\nStatus: " + status);
+//         });
+//     });
+// });
+
+var request = require('request');
+
+var dataString = 'text=great';
+
+var options = {
+    url: 'http://text-processing.com/api/sentiment/',
+    method: 'POST',
+    body: dataString
+};
+
+
+// var url = "http://text-processing.com/api/sentiment/";
+// var method = "POST";
+// var postData = "Some data";
+//
+// var shouldBeAsync = true;
+//
+// var request = new XMLHttpRequest();
+//
+// // Before we send anything, we first have to say what we will do when the
+// // server responds. This seems backwards (say how we'll respond before we send
+// // the request? huh?), but that's how Javascript works.
+// // This function attached to the XMLHttpRequest "onload" property specifies how
+// // the HTTP response will be handled.
+// request.onload = function () {
+//
+//    // Because of javascript's fabulous closure concept, the XMLHttpRequest "request"
+//    // object declared above is available in this function even though this function
+//    // executes long after the request is sent and long after this function is
+//    // instantiated. This fact is CRUCIAL to the workings of XHR in ordinary
+//    // applications.
+//
+//    // You can get all kinds of information about the HTTP response.
+//    var status = request.status; // HTTP response status, e.g., 200 for "200 OK"
+//    var data = request.responseText; // Returned data, e.g., an HTML document.
+// }
+//
+// request.open(method, url, shouldBeAsync);
+//
+// request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+// // Or... request.setRequestHeader("Content-Type", "text/plain;charset=UTF-8");
+// // Or... whatever
+//
+// // Actually sends the request to the server.
+// request.send(postData);
+//
+//
+// // // Imports the Google Cloud client library
+// // const language = require('@google-cloud/language');
+// //
+// // // Instantiates a client
+// // const client = new language.LanguageServiceClient();
+// //
+// // // The text to analyze
+// // const text = 'Hello, world!';
+// //
+// // const document = {
+// //   content: text,
+// //   type: 'PLAIN_TEXT',
+// // };
+// //
+// // // Detects the sentiment of the text
+// // client
+// //   .analyzeSentiment({document: document})
+// //   .then(results => {
+// //     const sentiment = results[0].documentSentiment;
+// //
+// //     console.log(`Text: ${text}`);
+// //     console.log(`Sentiment score: ${sentiment.score}`);
+// //     console.log(`Sentiment magnitude: ${sentiment.magnitude}`);
+// //   })
+// //   .catch(err => {
+// //     console.error('ERROR:', err);
+// //   });
